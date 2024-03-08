68f9fd9c96c6e33a6099b8e2edccd5aedd2ef2c2
Sivasangeeth <sivasangeeth04@gmail.com>
2024-03-08 22:58:53 +0530

    about us changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import img from "../Assets/about.jpg"
+import './about.css'
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='about-content' >
+      <img style={{width:'100%', height:'450px'}}src={img} alt='image'/>
+      <h1 className='centered'>About Us</h1>
+      <div className="mt-[50px] text-center max-w-4xl mx-auto">
+        <h4 className="text-4xl text-orange-500">COWORKING SPACE IN  DELHI</h4>
+        <div className="">
+        <p className="text-[20px] mt-3 text-justify">Flexspace provides excellent office spaces to their clients. The strategic positioning of Flexspace, Hargobind Enclave can be summed up as follows: 400 meters from Karkardooma metro station, on main Vikas Marg, 25 minutes from Connaught place, 20 minutes from Noida, 2 minutes from Energy gym and surrounded by variety of offices and eateries.
+        Flexspace, Delhi at Hargobind Enclave is a place for hardcore business community of entrepreneurs in  Delhi.</p>
+        </div>
+        {/* <h1>eew</h1> */}
+      </div>
+    </div>
   )
 }
