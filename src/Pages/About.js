f3ff32d0bb4f5793afb9cd80b9a3c4f7a77d396c
Solomon-03 <solomon172003@gmail.com>
2024-03-09 07:27:24 +0530

    pricing

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import Testomonial from '../Components/Testomonial'
-
-export const About = () => {
-  return (
-    <div>
-      <Testomonial />
-=======
 import img from "../Assets/about.jpg"
 import './about.css'
 export const About = () => {
@@ -20,9 +13,8 @@ export const About = () => {
         <p className="text-[20px] mt-3 text-justify">Flexspace provides excellent office spaces to their clients. The strategic positioning of Flexspace, Hargobind Enclave can be summed up as follows: 400 meters from Karkardooma metro station, on main Vikas Marg, 25 minutes from Connaught place, 20 minutes from Noida, 2 minutes from Energy gym and surrounded by variety of offices and eateries.
         Flexspace, Delhi at Hargobind Enclave is a place for hardcore business community of entrepreneurs in  Delhi.</p>
         </div>
-        {/* <h1>eew</h1> */}
       </div>
->>>>>>> 2b18c87dba0d1085673ab1e2dbe058a17a805ea9
+      <Testomonial />
     </div>
   )
 }
--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {Table, Container} from 'react-bootstrap'
-import { FcCheckmark } from "react-icons/fc";
-import { FaXmark } from "react-icons/fa6";
-import "./pricing.css"
+// import "../Style/pricing.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
 export const Pricing = () => {
   return (
-    <div className='pricing-table'>
+    <div className='pricing-table mt-5 p-4'>
       <Container>
       <Table striped bordered hover>
-      <thead>
-        <tr>
+      <thead data-aos="fade-left" data-aos-delay="100" data-aos-easing='ease-in-out' data-aos-duration="1000">
+        <tr data-aos="fade-left" data-aos-delay="200" data-aos-easing='ease-in-out' data-aos-duration="1000">
           <th>Plan</th>
           <th>Daily Pass</th>
           <th>Coworking Membership</th>
@@ -20,7 +20,7 @@ export const Pricing = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="300" data-aos-easing='ease-in-out' data-aos-duration="1000">
           <td>Coworking hours</td>
           <td>One time 24hours Access</td>
           <td>12hrs*7days*1month</td>
@@ -29,15 +29,15 @@ export const Pricing = () => {
           <td>12hrs*7days*1month</td>
 
         </tr>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="400" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <td>Meeting Rooms</td>
-          <td><FaXmark color='red' size={20}/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center' /></td>
           <td>6hours/month</td>
           <td>6hours/month</td>
-          <td><FaXmark color='red' size={20}/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center' /></td>
           <td>6hours/person</td>
         </tr>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="500" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <td>Addtional Meeting Rooms</td>
           <td>Rs.250/hour</td>
           <td>Rs.200/hour</td>
@@ -45,57 +45,57 @@ export const Pricing = () => {
           <td>Rs.200/hour</td>
           <td>Rs.200/hour</td>
         </tr>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="600" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <td>Timing</td>
           <td>24 hours</td>
           <td>9 am to 9 pm</td>
           <td>24 hours</td>
-          <td><FaXmark color='red' size={20}/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center' /></td>
           <td>24 hours</td>
         </tr>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="700" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <td>Key to building and rooms</td>
-          <td><FaXmark color='red' size={20} /></td>
-          <td><FaXmark color='red' size={20}/></td>
-          <td><FaXmark color='red' size={20}/></td>
-          <td><FaXmark color='red' size={20}/></td>
-          <td><FcCheckmark size={20}/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center' /></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center'/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center'/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center'/></td>
+          <td><FontAwesomeIcon icon={faCheckCircle} size={20}/></td>
         </tr>
-        <tr>
+        <tr data-aos="fade-left" data-aos-delay="800" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <td>Parking</td>
           <td>Rs.30 for 2 hours and Rs.50 for full day</td>
           <td>Rs.1000/month</td>
           <td>Rs.1000/month</td>
-          <td><FaXmark color='red' size={20}/></td>
+          <td><FontAwesomeIcon icon={faXmarkCircle} color='red' size={20} className='flex self-center' /></td>
           <td>Rs.1000/month</td>
         </tr>
       </tbody>
       
     </Table>
 
-    <form className="form">
-      <p className="title">Pricing </p>
-      {/* <p className="message">Signup now and get full access to our app. </p> */}
+    <form className="form mt-10">
+      <p className="title" data-aos="fade-down" data-aos-delay="50" data-aos-easing='ease-in-out' data-aos-duration="1000">Pricing </p>
+      <p className="message" data-aos="fade-down" data-aos-delay="100" data-aos-easing='ease-in-out' data-aos-duration="1000">Signup now and get full access to our app. </p>
       <div className="flex">
-        <label>
+        <label data-aos="fade-down" data-aos-delay="300" data-aos-easing='ease-in-out' data-aos-duration="1000">
           <input className="input" type="text" placeholder="" required="" />
           <span important className='span-field'>Firstname</span>
         </label>
-        <label>
+        <label data-aos="fade-down" data-aos-delay="400" data-aos-easing='ease-in-out' data-aos-duration="1000">
           <input className="input" type="text" placeholder="" required="" />
           <span className='span-field'>Lastname</span>
         </label>
       </div>
-      <label>
+      <label data-aos="fade-down" data-aos-delay="500" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <input className="input" type="email" placeholder="" required="" />
         <span className='span-field'>Email</span>
       </label>
-      <label>
+      <label data-aos="fade-down" data-aos-delay="500" data-aos-easing='ease-in-out' data-aos-duration="1000">
         <input className="input" type="text" placeholder="" required="" />
         <span className='span-field'>UPI Id</span>
       </label>
       
-      <button className="submit">Pay Bill</button>
+      <button data-aos="fade-down" data-aos-delay="700" data-aos-easing='ease-in-out' data-aos-duration="1000" className="submit">Pay Bill</button>
       
     </form>
 
